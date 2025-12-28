6015c9b6a175485c32b04bc3b10a9a4b9ec55739
SajedaHussain <sajedataqi22@gmail.com>
2025-12-28 10:01:05 +0300

    server

---
+++ server.js
@@ -0,0 +1,29 @@
+//dotenv
+const dotenv = require('dotenv');
+dotenv.config();//should be always in the top
+
+
+//mongoose
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI);//make connection with the url
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+
+
+//express
+const express = require('express');
+const app = express();
+
+
+//Middlewar
+const morgan = require('morgan');
+app.use(morgan('dev'));//dev is the comperhansiv show the log
+app.use(express.json());//tell express that will recive the json and pass beacuse will use API 
+
+
+// Routes go here
+app.listen(3000, () => {
+  console.log('The express app is ready!');
+});
